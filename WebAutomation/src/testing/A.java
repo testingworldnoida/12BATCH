1af60fae78e02a020730008e83d17d435215f46c
testingworldnoida <testingworldglobal@gmail.com>
2017-11-11 12:26:57 +0530

    Framework code started :  TASK -123

---
+++ WebAutomation/src/testing/A.java
@@ -0,0 +1,21 @@
+package testing;
+
+public class A {
+
+    public void getData(int  a)
+    {
+    	 //  for(int i=10; i<=10; i-- )
+    	 
+    	int i=1;
+    	
+    	while(i<=10)
+    	{
+    		System.out.println(a*i);
+    		i++;
+    	}
+    	
+    	
+    }
+
+	
+}
---
+++ WebAutomation/src/testing/B.java
@@ -0,0 +1,12 @@
+package testing;
+
+public class B {
+
+	public static void main(String[] args) {
+		
+		 A  o = new A();
+		 o.getData(117);
+		
+	}
+	
+}
